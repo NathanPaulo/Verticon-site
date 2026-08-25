c59d9595d979b3e09efd8c572a56a0bc2bbc2584
Nathan Paulo <nathanpaulo93@gmail.com>
2026-08-25 14:40:26 -0300

    Atualiza nome do site para Vertcon

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: '/Verticon-site/',
+  base: '/Vertcon-site/',
 
   server: {
     host: '0.0.0.0',
